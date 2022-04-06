ca8e654dbe455bbe5ee7e8823eb38f9acb06572c
57973 <57973@etu.he2b.be>
2022-04-06 18:11:17 +0200

    Etape 2.2 :  fonction builLevel et JsDoc

--- scripts/game.js
+++ scripts/game.js
@@ -1,1 +1,25 @@
-console.log("hello world");
+"use strict";
+/**
+ * Displays each character of the level given in parameter
+ * @param {number} level -Corresponds to the level number in the levels array(0 -> 6 included)
+ */
+function buildLevel(level) {
+        var laMap = levels[level].map;
+        for (let i = 0; i < laMap.length; i++) {
+                for (let j = 0; j < laMap[i].length; j++)
+                        console.log(laMap[i].charAt(j));
+
+        }
+}
+
+
+
+
+
+
+$( document ).ready(function() {
+        buildLevel;
+    });
+
+
+
