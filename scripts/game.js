b55db12a55edfad127458a1caecaaf28427973b3
burak <57973@etu.he2b.be>
2022-04-19 17:41:43 +0200

    fin ité 1  CORRIGER

--- scripts/game.js
+++ scripts/game.js
@@ -6,11 +6,29 @@
 function buildLevel(level) {
         var laMap = levels[level].map;
         for (let i = 0; i < laMap.length; i++) {
-                $("#world").addClass("square");
+
+                var divLigne = document.createElement('div');
+                $("#world").append(divLigne);
 
                 for (let j = 0; j < laMap[i].length; j++) {
-                        if(laMap[i].charAt(j)==" "){}
-
+                        var divCase = document.createElement('div');
+                        $(divLigne).append(divCase);
+                        $(divCase).addClass('squares');
+                        if(laMap[i].charAt(j)== " "){
+                                $(divCase).addClass('empty');
+                        }
+                        if(laMap[i].charAt(j) =='┌' || laMap[i].charAt(j) =='─' || laMap[i].charAt(j) =='┐' || laMap[i].charAt(j) =='│' || laMap[i].charAt(j) =='└' || laMap[i].charAt(j) =='┘' || laMap[i].charAt(j) =='═'|| laMap[i].charAt(j) =='╷' || "├" ){
+                                $(divCase).addClass('wall');
+                        }
+                        if(laMap[i].charAt(j)=='#'){
+                                $(divCase).addClass('box');
+                        }
+                        if(laMap[i].charAt(j) == 'x'){
+                                $(divCase).addClass('target');
+                        }
+                        if(laMap[i].charAt(j) == '🧍'){
+                                $(divCase).addClass('player');
+                        }
                 }
 
         }
@@ -21,6 +39,3 @@ function buildLevel(level) {
 $(document).ready(function () {
         buildLevel;
 });
-
-
-
