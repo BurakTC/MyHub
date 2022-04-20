0c5882f3b6da178fcd639b57f1bf56c4365ee783
burakcan <57973@etu.he2b.be>
2022-04-20 23:19:08 +0200

    Build level +- OK //Corriger l'affichage mirroir

--- scripts/game.js
+++ scripts/game.js
@@ -5,34 +5,95 @@
  */
 function buildLevel(level) {
         var laMap = levels[level].map;
+
+
         for (let i = 0; i < laMap.length; i++) {
 
                 var divLigne = document.createElement('div');
                 $("#world").append(divLigne);
 
-                for (let j = 0; j < laMap[i].length; j++) {
+
+
+
+                for (var c of laMap[i]) {
                         var divCase = document.createElement('div');
+
                         $(divLigne).append(divCase);
-                        $(divCase).addClass('squares');
-                        if(laMap[i].charAt(j)== " "){
+                        $(divCase).addClass('square');
+                        if (c == " ") {
                                 $(divCase).addClass('empty');
                         }
-                        if(laMap[i].charAt(j) =='┌' || laMap[i].charAt(j) =='─' || laMap[i].charAt(j) =='┐' || laMap[i].charAt(j) =='│' || laMap[i].charAt(j) =='└' || laMap[i].charAt(j) =='┘' || laMap[i].charAt(j) =='═'|| laMap[i].charAt(j) =='╷' || "├" ){
-                                $(divCase).addClass('wall');
-                        }
-                        if(laMap[i].charAt(j)=='#'){
+                       // if (c == '┌' || c == '─' || c == '┐' || c == '│' || c == '└' || c == '┘' || c == '═' || c == '╷' || c == "├" ||c=='┬' || c== '╶'||c=='┤') {
+                       //         $(divCase).addClass('wall');
+                      //  }
+                        else if (c == '#') {
                                 $(divCase).addClass('box');
                         }
-                        if(laMap[i].charAt(j) == 'x'){
+                        else if (c == 'x') {
                                 $(divCase).addClass('target');
                         }
-                        if(laMap[i].charAt(j) == '🧍'){
+                        else if (c == '🧍') {
                                 $(divCase).addClass('player');
                         }
+                        else if (c == '@') {
+                                $(divCase).addClass('box');
+                                $(divCase).addClass('target');
+                        }
+                        else {
+                                $(divCase).addClass('wall');
+
+                        }
+
+
+
                 }
+
+                // switch (laMap[i].charAt(j)) {
+
+                //         case 'x':
+                //                 $(divCase).addClass('target');
+                //                 break;
+                //         case '#':
+                //                 $(divCase).addClass('box');
+                //                 break;
+                //         case " ":
+                //                 $(divCase).addClass('empty');
+                //                 break;
+                //         case '┌':
+                //                 $(divCase).addClass('wall');
+                //                 break;
+                //         case '─':
+                //                 $(divCase).addClass('wall');
+                //                 break
+                //         case '┐':
+                //                 $(divCase).addClass('wall');
+                //                 break;
+                //         case '│':
+                //                 $(divCase).addClass('wall');
+                //                 break;
+                //         case '└':
+                //                 $(divCase).addClass('wall');
+                //                 break;
+                //         case '┘':
+                //                 $(divCase).addClass('wall');
+                //                 break;
+                //         case '═':
+                //                 $(divCase).addClass('wall');
+                //                 break;
+                //         case '╷':
+                //                 $(divCase).addClass('wall');
+                //                 break
+                //         case "├":
+                //                 $(divCase).addClass('wall');
+                //                 break;
+                //         default:
+                //                 $(divCase).addClass('player');
+                //                 break;
+                // }
 
         }
 }
+
 
 
 
