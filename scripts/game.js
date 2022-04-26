31975cc394806595712e9173e38f26fce1ef204c
57973 <57973@etu.he2b.be>
2022-04-26 15:53:44 +0200

    Ajout des methodes getSquareAt et getPlayerPosition

--- scripts/game.js
+++ scripts/game.js
@@ -6,13 +6,21 @@
 function buildLevel(level) {
         var laMap = levels[level].map;
 
-        for (var lignes of laMap) {
+
+        for (var lignes of laMap) {  //Creation des lignes
 
                 var divLigne = document.createElement('div');
                 $("#world").append(divLigne);
                 $(divLigne).addClass('Lignes')
-                for (var c of lignes) {
+
+
+                for (var c of lignes) {  //Creation des cases
+
+
+
                         var divCase = document.createElement('div');
+
+
 
                         $(divLigne).append(divCase);
                         $(divCase).addClass('square');
@@ -37,12 +45,63 @@ function buildLevel(level) {
                                 $(divCase).addClass('wall');
 
                         }
+
+
                 }
         }
 }
 
 
 
+
+function getPlayerPosition() {
+        let laMap = $("#world");
+        let i = 0;
+        for (let lignes of laMap.children()) {
+                let j = 0;
+                for (let cases of $(lignes).children()) {
+                        let position = {
+                                x: i,
+                                y: j
+                        }
+                        if (cases.classList.contains("player")) {
+                                console.log(position);
+                        }
+                        j = j + 1;
+                }
+                i = i + 1;
+        }
+}
+
+/**
+ * @param {{ x: any; y: any; }} position
+ */ //Donner un objet en parametre ex: -> getSquareAt({x:5,y:6})
+function getSquareAt(position) {
+        let laMap = $("#world");
+        var ligne = laMap.children().eq(position.x);
+
+        var square = $(ligne).children().eq(position.y);
+        console.log(square)
+        if (square.hasClass("player")) {
+                console.log("Le joueur");
+        }
+        if (square.hasClass("box")) {
+                console.log("Une boite");
+        }
+        if (square.hasClass("wall")) {
+                console.log("Un mur");
+        }
+        if (square.hasClass("empty")) {
+                console.log("Case libre");
+        }
+        if (square.hasClass("target")) {
+                console.log("Une cible");
+        }
+        if (square.hasClass("box")&&square.hasClass("target")) {
+                console.log("Une boite sur une cible");
+        }
+
+}
 
 
 
